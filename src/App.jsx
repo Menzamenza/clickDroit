5aca5df970c8d37d90b634e387325393cf385ace
Menza <menzamenza90@gmail.com>
2025-05-26 19:50:58 +0000

    erreur de casse avec home qui devient Acceuil

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import CreationEntreprise from './pages/CreationEntreprise';
-import Home from './pages/home';
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
+import Home from './pages/Accueil';
+
+
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Home/>} />
         <Route path="/creation" element={<CreationEntreprise />} /> 
         {/* <Route path="/services" element={<Services />} /> etc.pour les autres pages */}
       </Routes>
